bf0a82b43101f9eb36c3bc71b2da8472d01a7e8c
Matt Diamant <matthewdiamant@gmail.com>
2022-08-14 16:56:53 -0700

    random cpu unit movement

--- src/CPUPlayer.js
+++ src/CPUPlayer.js
@@ -1,5 +1,7 @@
 import Player from "./Player";
 
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 class CPUPlayer extends Player {
   constructor() {
     super();
@@ -8,7 +10,16 @@ class CPUPlayer extends Player {
     this.addBuilding({ type: "base", x: 80 * 12, y: 80 * 4 });
   }
 
+  cpuActions() {
+    if (Math.random() < 0.01) {
+      const unit = sample(this.units);
+      const path = [80 * 8 + 80 * Math.random() * 5, 80 * Math.random() * 7];
+      unit.setPath(path);
+    }
+  }
+
   tick() {
+    this.cpuActions();
     Player.tick.call(this);
   }
 }
